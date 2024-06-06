ef2b8ce13bde618c9f1cb1b8e285f0e3dfc3697a
Mritunjay021 <mrityurk21@gmail.com>
2024-06-06 19:58:24 +0530

    java

---
+++ D.java
@@ -0,0 +1,28 @@
+interface A{
+void methodA();
+}
+interface B{
+void methodB();
+}
+interface C extends A,B{
+void methodC();
+}
+class D implements C{
+public void methodA(){
+System.out.println("Implemetation of methodA");
+}
+public void methodB(){
+System.out.println("Implementation of methodB");
+}
+public void methodC(){
+System.out.println("Implementation of methodC");
+}
+}
+ class extdinterface{
+public static void main(String[]args){
+D obj=new D();
+obj.methodA();
+obj.methodB();
+obj.methodC();
+}
+}
