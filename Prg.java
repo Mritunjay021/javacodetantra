81471fb16225c850b80ccda29957cc7a9511a6dc
Mritunjay021 <mrityurk21@gmail.com>
2024-08-02 16:55:32 +0530

    Create Prg.java

---
+++ Prg.java
@@ -0,0 +1,16 @@
+public class Prg
+{
+int a,b;
+Prg(int a)
+{
+System.out.println(a);
+}
+Prg(int a,int b)
+{
+System.out.println(a+" "+b);
+}
+public static void main(String[]args)
+{
+Program n=new Prg(2,3);
+}
+}
